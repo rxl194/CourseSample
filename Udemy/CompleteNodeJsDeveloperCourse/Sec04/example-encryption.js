f17d78fe14da7a9b2c15053c4427a4b9df24c20d
rxl194 <Rendong_Lin@yahoo.com>
2015-12-03 14:36:17 -0500

    Lecture 57

---
+++ Udemy/CompleteNodeJsDeveloperCourse/Sec04/example-encryption.js
@@ -0,0 +1,20 @@
+var crypto = require('crypto-js');
+
+var secretMessage = {
+  name: 'Andrew',
+  secretName: '007'
+};
+var secretKey = '123abc';
+
+// Encrypt
+var encryptedMessage = crypto.AES.encrypt(JSON.stringify(secretMessage), secretKey);
+console.log('Encrypted Message: ' + encryptedMessage);
+
+
+// Decrypt Message
+var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
+var decryptedMessage = JSON.parse(bytes.toString(crypto.enc.Utf8));
+console.log('Decrypted Message: ' + decryptedMessage);
+console.log('Decrypted Message name: ' + decryptedMessage.secretName);
+
+
