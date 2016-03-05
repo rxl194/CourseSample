0db78463f17e6838f7086a501f831b34aa290bb9
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-03-05 12:47:06 -0800

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

---
+++ packtpub/AngularJS Test-driven Development/3/chromeOnlyConf.js
@@ -0,0 +1,25 @@
+// An example configuration file.
+exports.config = {
+  // Do not start a Selenium Standalone sever - only run this using chrome.
+  chromeOnly: true,
+  chromeDriver: 'C:/Users/renlin/AppData/Roaming/npm/node_modules/protractor/selenium/chromedriver.exe',
+  
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8080/',
+  
+  // Spec patterns are relative to the current working directly when
+  // protractor is called.
+  // specs: ['confirmConfigTest.js'],
+    specs: ['spec/e2e/**/*.js'],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  }
+};
